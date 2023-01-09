91d56be6932113f772b2f67d3fbbf7bc8749aafb
Avisha0601 <86399682+Avisha0601@users.noreply.github.com>
2023-01-09 20:46:22 +0530

    Add files via upload

---
+++ python01.py
@@ -0,0 +1,42 @@
+def add_numbers(num1, num2, num3, num4, num5):
+    sum = num1 + num2 + num3 + num4 + num5
+    print("Sum: ",sum)
+    
+add_numbers(277.8625, 371.4906, 437.8920, 963.2478, 185.627)
+
+
+def subtract_numbers(num1, num2, num3, num4, num5):
+    difference = num1 - num2 - num3 - num4 - num5
+    print("Difference: ",difference)
+    
+subtract_numbers(277.8625, 371.4906, 437.8920, 963.2478, 185.627)
+
+
+def multiply_numbers(num1, num2, num3, num4, num5):
+    product = num1 * num2 * num3 * num4 * num5
+    print("Product: ",product)
+    
+multiply_numbers(277.8625, 371.4906, 437.8920, 963.2478, 185.627)
+
+
+def divide_numbers(num1, num2, num3, num4, num5):
+    quotient = num1 / num2 / num3 / num4 / num5
+    print("Quotient: ",quotient)
+    
+divide_numbers(277.8625, 371.4906, 437.8920, 963.2478, 185.627)
+
+
+def find_square(num):
+    result = num * num 
+    return result
+
+square = find_square(5682201)
+print('Square:',square)
+
+
+def find_cube(num):
+    result = num * num * num
+    return result
+
+cube = find_cube(56822091)
+print('Cube:',cube)
